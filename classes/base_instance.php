957b7a126819ecead79240cce934945eff44e4cc
Umut Zerman <Umut.zerman@schulen.nuernberg.de>
2026-03-08 03:37:51 +0100

    MBS-10591: Fall back to default tenant when saving empty tenant string

--- classes/base_instance.php
+++ classes/base_instance.php
@@ -580,7 +580,8 @@ class base_instance {
         $this->set_apikey(!empty($data->apikey) ? trim($data->apikey) : '');
         $this->set_useglobalapikey(!empty($data->useglobalapikey));
         $this->set_connector($data->connector);
-        $this->set_tenant(trim($data->tenant));
+        $tenantvalue = trim($data->tenant);
+        $this->set_tenant(empty($tenantvalue) ? tenant::DEFAULT_IDENTIFIER : $tenantvalue);
         if (empty($data->model)) {
             // This is only a fallback. If the connector does not support the selection of a model,
             // it is supposed to overwrite this default value in the extend_store_formdata function.
